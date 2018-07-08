6867cfa2a59f2066690bffb9fb67503d4449dc61
Judit Koszo <koszojudit@hotmail.com>
2018-07-08 23:58:34 +0200

    Details page - styling and responsive design with CSS

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -19,22 +19,22 @@ initMap = () => {
       self.newMap = L.map('map', {
         center: [restaurant.latlng.lat, restaurant.latlng.lng],
         zoom: 16,
-        scrollWheelZoom: false
+        scrollWheelZoom: true
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1Ijoia29zem9qdWRpdCIsImEiOiJjampiYWtxZ24zZXRhM3JzMmZ6eXprNmVnIn0.p6pBCGHEkLXx86_R10utkw',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
           'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
         id: 'mapbox.streets'
       }).addTo(newMap);
-      fillBreadcrumb();
+
       DBHelper.mapMarkerForRestaurant(self.restaurant, self.newMap);
     }
   });
 }
- 
+
 /**
  * Get current restaurant from page URL.
  */
@@ -67,15 +67,22 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
   const name = document.getElementById('restaurant-name');
   name.innerHTML = restaurant.name;
 
+  const neighborhood = document.getElementById('restaurant-neighborhood');
+  neighborhood.innerHTML = restaurant.neighborhood;
+
   const address = document.getElementById('restaurant-address');
   address.innerHTML = restaurant.address;
 
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img'
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.setAttribute('aria-label', 'Image of ' + restaurant.name + ' restaurant, ');
 
   const cuisine = document.getElementById('restaurant-cuisine');
   cuisine.innerHTML = restaurant.cuisine_type;
+
+  const rating = document.getElementById('restaurant-rating');
+  rating.innerHTML = 'Rating: ' + restaurantRating(restaurant);
 
   // fill operating hours
   if (restaurant.operating_hours) {
@@ -84,6 +91,14 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
   // fill reviews
   fillReviewsHTML();
 }
+
+restaurantRating = (restaurant) => {
+  let reviews = restaurant.reviews.map( (r) => r.rating);
+  let rating = reviews.reduce((a, b) => a + b, 0) / reviews.length;
+  rating = rating.toFixed(1);
+
+  return rating;
+};
 
 /**
  * Create restaurant operating hours HTML table and add it to the webpage.
@@ -132,33 +147,32 @@ fillReviewsHTML = (reviews = self.restaurant.reviews) => {
  */
 createReviewHTML = (review) => {
   const li = document.createElement('li');
+
+  const heading = document.createElement('div');
+  heading.className = 'review-heading';
+  li.appendChild(heading);
+
   const name = document.createElement('p');
   name.innerHTML = review.name;
-  li.appendChild(name);
+  name.className = 'name';
+  heading.appendChild(name);
 
   const date = document.createElement('p');
   date.innerHTML = review.date;
-  li.appendChild(date);
+  date.className = 'date';
+  heading.appendChild(date);
 
   const rating = document.createElement('p');
   rating.innerHTML = `Rating: ${review.rating}`;
+  rating.className = 'rating';
   li.appendChild(rating);
 
   const comments = document.createElement('p');
   comments.innerHTML = review.comments;
+  comments.className = 'comment';
   li.appendChild(comments);
 
   return li;
-}
-
-/**
- * Add restaurant name to the breadcrumb navigation menu
- */
-fillBreadcrumb = (restaurant=self.restaurant) => {
-  const breadcrumb = document.getElementById('breadcrumb');
-  const li = document.createElement('li');
-  li.innerHTML = restaurant.name;
-  breadcrumb.appendChild(li);
 }
 
 /**
